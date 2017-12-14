faf2c66419aa7b6a162c174d4d878bdf574d3176
zhy <heyongzhang@outlook.com>
2017-12-14 20:11:22 +0800

    Add OS C/V code as well as markdown explanation

--- OS/MultiServer.py
+++ OS/MultiServer.py
@@ -1,15 +1,36 @@
-import socket, math
+import socket
 from threading import Thread
+
+
+def factorial(x):
+    if x == 1 or x == 0:
+        return 1
+    else:
+        return x * factorial(x - 1)
+#执行线程的函数
+
+
+def function(msg, cs):
+    ans = str(factorial(int(msg))).encode('utf-8')
+    try:
+        cs.send(ans)
+    except:
+        pass
+
+#分配输入的函数
+
 
 def f(cs, name):
     cs.send("Log in Server！".encode('utf-8'))
     msg = cs.recv(1024).decode('utf-8')
     while msg != 'bye':
-        ans = str(math.factorial(int(msg)))
-        cs.send(ans.encode('utf-8'))
+      	#为用户的每个输入分配一个线程
+        calculate_thread = Thread(target=function, args=(msg, cs))
+        calculate_thread.start()
         msg = cs.recv(1024).decode('utf-8')
     print('Client', name, 'log out')
     cs.close()
+
 
 def initSever(host, port):
     ss = socket.socket()
@@ -22,8 +43,10 @@ def initSever(host, port):
         print('Connecting to', addr)
         name = str(num)
         num += 1
+        #为每个用户分配一个线程
         client_thread = Thread(target=f, args=(cs, name))
         client_thread.start()
+
 
 if __name__ == '__main__':
     initSever(socket.gethostname(), 5458)
